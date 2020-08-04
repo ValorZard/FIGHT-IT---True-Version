5ba1ec230304021176141eb06951a9439fe29d11
Srayan Jana <srayanj@uci.edu>
2020-08-03 22:47:42 -0700

    Added "shooting"
    its unholy and really really shitty

--- resources/Assets/Bullets/default_bullet.gd
+++ resources/Assets/Bullets/default_bullet.gd
@@ -19,4 +19,10 @@ func _physics_process(delta):
 	move_and_collide(bullet_speed * bullet_direction * delta)
 	pass
 
+func on_hit(body):
+	self.queue_free()
+	pass
 
+func _on_Area2D_body_entered(body):
+	on_hit(body)
+	pass # Replace with function body.
